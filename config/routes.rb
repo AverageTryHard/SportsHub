be20c1fe232d050f857b01aacf771e24769a1fb8
mhomz <mikhailgomziak@gmail.com>
2021-06-01 16:52:04 +0300

    User management moved

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   device_for :users
   get :admin_layout, to: 'admin_layout#admin_layout', path: 'admin-cabinet'
   get :user_layout, to: 'user_layout#user_layout', path: 'main-page'
-  root "admin_layout#admin_layout"
+  root 'admin_layout#admin_layout'
 end
