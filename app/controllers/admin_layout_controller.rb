892dafecc1aefdcd74bbea019c2897de62a1137e
mhomz <mikhailgomziak@gmail.com>
2021-06-03 14:59:04 +0300

    Improved layouts

--- app/controllers/admin_layout_controller.rb
+++ app/controllers/admin_layout_controller.rb
@@ -1,2 +1,9 @@
-class AdminLayoutController < ActionController::Base
+class AdminLayoutController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name password password_confirmation])
+  end
 end
--- app/controllers/user_layout_controller.rb
+++ app/controllers/user_layout_controller.rb
@@ -1,3 +1,9 @@
-class UserLayoutController < ActionController::Base
+class UserLayoutController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name password password_confirmation])
+  end
 end
