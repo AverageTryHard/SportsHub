7c4d14d530aad35f73ac314f097be73f60f6b56c
mhomz <mikhailgomziak@gmail.com>
2021-06-22 15:37:10 +0300

    User partial fix. Correct request type for status change methods. Added traits and let statement.

--- spec/requests/admin/user_spec.rb
+++ spec/requests/admin/user_spec.rb
@@ -3,38 +3,58 @@ require 'rails_helper'
 RSpec.describe 'Admin::Users', type: :request do
   describe 'GET /admin' do
     it 'Admin gets access to admin cabinet' do
-      admin = build(:user, is_admin: true)
-      admin.confirm
+      admin = create(:confirmed_admin)
       sign_in admin
       get admin_root_path
       expect(response.status).to eq(200)
     end
 
     it 'User gets access to admin cabinet' do
+      user = create(:confirmed_user)
+      sign_in user
+      get admin_root_path
+      expect(response.status).to eq(302)
+    end
+
+    it 'Guest gets access to admin cabinet' do
       get admin_root_path
       expect(response.status).to eq(302)
     end
   end
 
-  describe 'GET /admin/user' do
-    before do
-      admin = create(:user, is_admin: true)
-      admin.confirm
-      sign_in admin
-    end
+  describe 'PUT /admin/block_user' do
+    let!(:user_for_test) { create(:confirmed_user) }
 
-    it 'User block' do
-      user_for_test = build(:user)
-      user_for_test.confirm
-      get admin_block_user_path(id: user_for_test)
+    it 'Admin block user' do
+      admin = create(:confirmed_admin)
+      sign_in admin
+      put admin_block_user_path(id: user_for_test)
       expect(user_for_test.reload.status).to eq('blocked')
     end
 
-    it 'User activation' do
-      user_for_test = build(:user, status: 'blocked')
-      user_for_test.confirm
-      get admin_activate_user_path(id: user_for_test)
+    it 'User block other user' do
+      user = create(:confirmed_user)
+      sign_in user
+      put admin_block_user_path(id: user_for_test)
       expect(user_for_test.reload.status).to eq('active')
+    end
+  end
+
+  describe 'PUT /admin/activate_user' do
+    let!(:user_for_test) { create(:confirmed_user, status: 'blocked') }
+
+    it 'Admin activate user' do
+      admin = create(:confirmed_admin)
+      sign_in admin
+      put admin_activate_user_path(id: user_for_test)
+      expect(user_for_test.reload.status).to eq('active')
+    end
+
+    it 'User activate other user' do
+      user = create(:confirmed_user)
+      sign_in user
+      put admin_activate_user_path(id: user_for_test)
+      expect(user_for_test.reload.status).to eq('blocked')
     end
   end
 end
