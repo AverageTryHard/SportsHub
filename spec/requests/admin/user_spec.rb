aa25e1a535d37d48235c0e51dcecda4e426fd6b3
mhomz <mikhailgomziak@gmail.com>
2021-06-23 17:19:41 +0300

    Database cleaner added and fixed way of using let statement.

--- spec/requests/admin/user_spec.rb
+++ spec/requests/admin/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin::Users', type: :request do
+  let(:admin) { create(:confirmed_admin) }
+  let(:active_user) { create(:confirmed_user) }
+  let(:blocked_user) { create(:confirmed_user, status: 'blocked') }
+
   describe 'GET /admin' do
     it 'Admin gets access to admin cabinet' do
-      admin = create(:confirmed_admin)
       sign_in admin
       get admin_root_path
       expect(response.status).to eq(200)
@@ -23,38 +26,34 @@ RSpec.describe 'Admin::Users', type: :request do
   end
 
   describe 'PUT /admin/block_user' do
-    let!(:user_for_test) { create(:confirmed_user) }
-
     it 'Admin block user' do
       admin = create(:confirmed_admin)
       sign_in admin
-      put admin_block_user_path(id: user_for_test)
-      expect(user_for_test.reload.status).to eq('blocked')
+      put admin_block_user_path(id: active_user)
+      expect(active_user.reload.status).to eq('blocked')
     end
 
     it 'User block other user' do
       user = create(:confirmed_user)
       sign_in user
-      put admin_block_user_path(id: user_for_test)
-      expect(user_for_test.reload.status).to eq('active')
+      put admin_block_user_path(id: active_user)
+      expect(active_user.reload.status).to eq('active')
     end
   end
 
   describe 'PUT /admin/activate_user' do
-    let!(:user_for_test) { create(:confirmed_user, status: 'blocked') }
-
     it 'Admin activate user' do
       admin = create(:confirmed_admin)
       sign_in admin
-      put admin_activate_user_path(id: user_for_test)
-      expect(user_for_test.reload.status).to eq('active')
+      put admin_activate_user_path(id: blocked_user)
+      expect(blocked_user.reload.status).to eq('active')
     end
 
     it 'User activate other user' do
       user = create(:confirmed_user)
       sign_in user
-      put admin_activate_user_path(id: user_for_test)
-      expect(user_for_test.reload.status).to eq('blocked')
+      put admin_activate_user_path(id: blocked_user)
+      expect(blocked_user.reload.status).to eq('blocked')
     end
   end
 end
