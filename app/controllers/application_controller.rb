5ea806f22245cf2e223b0f8450cf6716c930fb3c
mhomz <mikhailgomziak@gmail.com>
2021-06-30 17:31:24 +0300

    Added helper method checker for attached avatar. Minor fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name password password_confirmation
-                                                         avatar avatar_cache remove_avatar])
+                                                         avatar])
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar,
                :avatar_cache, :remove_avatar)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_layout
     controller.send :_layout, lookup_context, []
   end
+
+  def render_user_avatar
+    if current_user.avatar.attached?
+      image_tag(current_user.avatar, class: 'rounded-circle', size: '50x50')
+    else
+      image_tag('avatar.jpg', class: 'rounded-circle', size: '50x50')
+    end
+  end
 end
