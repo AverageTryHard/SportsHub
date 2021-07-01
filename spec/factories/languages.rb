342694f29b3d1256779345ae943ec32f3873811c
mhomz <mikhailgomziak@gmail.com>
2021-07-01 08:18:20 +0300

    is_active inclusion test fix.

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :language do
     language_name { FFaker::String }
     locale_name { 'EN' }
-    is_active { 'active' }
+    is_active { true }
   end
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Language, type: :model do
 
     it { is_expected.to validate_presence_of(:language_name) }
     it { is_expected.to validate_presence_of(:locale_name) }
-    it { is_expected.to validate_presence_of(:is_active) }
+    it { is_expected.to validate_inclusion_of(:is_active).in_array([true, false]) }
     it { is_expected.to validate_length_of(:locale_name).is_equal_to(2) }
   end
 end
